0661dcb010f79b76749af461bfe2d76d05b837f4
cheemaladineemahima <cheemaladinnemahima@gmail.com>
2024-02-15 21:41:16 +0530

    Register page changes

--- Screens/Authn/LoginScreen.tsx
+++ Screens/Authn/LoginScreen.tsx
@@ -1,29 +1,67 @@
-import { TouchableOpacity, 
-    Text, 
-    StyleSheet, 
-    GestureResponderEvent, 
-    Alert, 
-    View 
+import {
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    GestureResponderEvent,
+    Alert,
+    View
 } from "react-native";
+import React, { useState, createRef } from 'react';
+import { TextInput } from "react-native-gesture-handler";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { blue } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 
-const LoginScreen = ({navigation}) => {
+const LoginScreen = ({ navigation }) => {
 
-    function handleSubmitPress(event: GestureResponderEvent): void {
-        Alert.alert('Navigation', 'Login button clicked');
+    const [intValue, setIntValue] = useState(0);
+    const [strungValue, setStringValue] = useState('');
+    const [boolValue, setBoolValue] = useState(false);
+
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    
+    
+    
+
+    
+   
+    function handleSubmitPress(): void {
+        if (!name) {
+            Alert.alert("Invalid", "Enter User Name");
+            return;
+        }
+        if (!password) {
+            Alert.alert("Invalid", "Enter password");
+            return;
+        }
+        Alert.alert("Valid", "Success!");
+        //AsyncStorage.setItem('user_id', name);
     }
 
-    function handleNavigationPress(event: GestureResponderEvent): void {
+    function handleNavigationPress(): void {
         navigation.navigate('RegisterScreen')
     }
 
     return (
         <View>
             <Text>Welcome to Login</Text>
+
+            <TextInput placeholder="Enter Name" onChangeText={(name) => {
+                console.log(name);
+                setName(name);
+            }} style={{ borderColor: '#ff0000' }} />
+            <Text style={{fontWeight:'bold', color:'blue',fontSize:20}}>{name}</Text>
+            <TextInput placeholder="Enter Password" onChangeText={(password) => {
+                console.log(password);
+                setPassword(password);
+            }} style={{ borderColor: '#EDC7C6' }} />
+            <Text style={{fontWeight:'bold', color:'blue',fontSize:20}}>{password}</Text>
+            
             <TouchableOpacity
                 style={styles.buttonStyle}
                 activeOpacity={0.5}
                 onPress={handleSubmitPress}>
-                <Text style={styles.buttonTextStyle}>LOGIN</Text>
+                <Text style={styles.buttonTextStyle}>USER LOGIN</Text>
             </TouchableOpacity>
             <TouchableOpacity
                 style={styles.buttonStyle}
--- Screens/Authn/SplashScreen.tsx
+++ Screens/Authn/SplashScreen.tsx
@@ -6,19 +6,17 @@ const SplashScreen = ({ navigation }) => {
     const [animating, setAnimating] = useState(true);
 
     useEffect(() => {
-        setTimeout(() => {
-            setAnimating(false);
-            //Check if user_id is set or not
-            //If not then send for Authentication
-            //else send to Home Screen
-            AsyncStorage.getItem('user_id').then((value) =>
-                navigation.replace(
-                    value === null ? 'Authn' : 'Dashboard'
-                ),
-            );
-        }, 5000);
+        console.log("UseEffectCalling");
+        setAnimating(false);
+        //Check if user_id is set or not
+        //If not then send for Authentication
+        //else send to Home Screen
+        AsyncStorage.getItem('userid').then((value) =>
+            navigation.replace(
+                value === null ? 'Authn' : 'Dashboard'
+            ),
+        );
     }, []);
-
 
     return (
         <View style={styles.container}>
